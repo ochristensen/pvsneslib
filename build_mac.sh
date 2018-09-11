c8d05ab9bab467f8c530834065f5f6a5c8a1ac7f
Owen Christensen <ochristensen@minitab.com>
2018-09-11 10:06:29 -0400

    try to supress homebrew autoupdate

--- build_mac.sh
+++ build_mac.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
-HOMEBREW_NO_AUTO_UPDATE=1
-brew install ninja
-brew upgrade cmake
+HOMEBREW_NO_AUTO_UPDATE=1 brew install ninja
+HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade cmake
 ./cmake_generate.sh build Release
 cd build
 ninja

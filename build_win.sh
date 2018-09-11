43cbb9a87cd16ae1404b104c0992911dec7ec1d6
Owen Christensen <ochristensen@minitab.com>
2018-09-11 10:59:42 -0400

    curl ninja-win

---
+++ build_win.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+curl -L https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip > ninja-win.zip
+unzip -oqq ninja-win.zip
+./cmake_generate.sh build Release
+cd build
+../ninja

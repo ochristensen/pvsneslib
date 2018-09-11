bf2a9707c72fa0fe37a3ccc9d3d1bc402817b119
owen christensen <humanitas@gmail.com>
2018-09-10 22:43:32 -0400

    update

--- build_linux.sh
+++ build_linux.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-6.0 main" | sudo tee -a /etc/apt/sources.list >/dev/null
 sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
 sudo -E apt-get -yq update
 sudo -E apt-get -yq --no-install-suggests --no-install-recommends install cmake ninja-build clang++-6.0
-CC=clang-6.0
-CXX=clang++-6.0
+export CC=clang-6.0
+export CXX=clang++-6.0
 ./cmake_generate.sh build Release
 cd build
 ninja

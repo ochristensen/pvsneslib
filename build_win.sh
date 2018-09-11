bf0c3cd416890b69d5cde56a8c608e40b14b7555
Owen Christensen <ochristensen@minitab.com>
2018-09-11 13:14:23 -0400

    update

--- build_win.sh
+++ build_win.sh
@@ -2,7 +2,7 @@
 
 curl -L https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip > ninja-win.zip
 unzip -oqq ninja-win.zip
-export PATH=$PATH:"pwd"
+export PATH="$(pwd)":$PATH
 ./cmake_generate.sh build Release
 cd build
 ../ninja

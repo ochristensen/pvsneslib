82311e7826fe7b83976832b64c9a76df7ddb712d
Owen Christensen <ochristensen@minitab.com>
2018-09-11 15:17:47 -0400

    use cl

--- build_win.sh
+++ build_win.sh
@@ -3,6 +3,8 @@
 curl -L https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip > ninja-win.zip
 unzip -oqq ninja-win.zip
 export PATH="$(pwd)":$PATH
+export CC=cl
+export CXX=cl
 ./cmake_generate.sh build Release
 cd build
 ../ninja

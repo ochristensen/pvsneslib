dee128acb7712f30b89553c9314ddba3b5df3024
owen christensen <humanitas@gmail.com>
2018-09-10 22:24:22 -0400

    update

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-6.0 main" | sudo tee -a /etc/apt/sources.list >/dev/null
 sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
 sudo -E apt-get -yq update

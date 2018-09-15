ab77c7f8a2073b80b995e0eb7723e4ebd1c225bb
owen christensen <humanitas@gmail.com>
2018-09-15 16:10:44 -0400

    try specific version

--- build_win.sh
+++ build_win.sh
@@ -3,7 +3,7 @@
 curl -L https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip > ninja-win.zip
 unzip -o ninja-win.zip
 export PATH="$(pwd)":$PATH
-export PATH="/c/Program\ Files\ \(x86\)/Windows\ Kits/10/bin/x64":$PATH
+export PATH="/c/Program\ Files\ \(x86\)/Windows\ Kits/10/bin/10.0.15063.0/x64":$PATH
 export tools_ver=$(cat /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Auxiliary/Build/Microsoft.VCToolsVersion.default.txt)
 export CC=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Tools/MSVC/$tools_ver/bin/Hostx64/x64/cl.exe
 export CXX=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Tools/MSVC/$tools_ver/bin/Hostx64/x64/cl.exe

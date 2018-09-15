6ee08102dd0880a78a759cd9bce4679bdae17d27
owen christensen <humanitas@gmail.com>
2018-09-15 13:25:39 -0400

    keep exporting path

--- build_win.sh
+++ build_win.sh
@@ -2,6 +2,7 @@
 
 curl -L https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip > ninja-win.zip
 unzip -o ninja-win.zip
+export PATH="$(pwd)":$PATH
 export tools_ver=$(cat /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Auxiliary/Build/Microsoft.VCToolsVersion.default.txt)
 export CC=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Tools/$tools_ver/bin/Hostx64/x64/cl.exe
 export CXX=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Tools/$tools_ver/bin/Hostx64/x64/cl.exe

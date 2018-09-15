6e3ac9655b38ba402e7a63b1e522295799729e7f
owen christensen <humanitas@gmail.com>
2018-09-15 13:28:05 -0400

    forgot part of path

--- build_win.sh
+++ build_win.sh
@@ -4,8 +4,8 @@ curl -L https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.
 unzip -o ninja-win.zip
 export PATH="$(pwd)":$PATH
 export tools_ver=$(cat /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Auxiliary/Build/Microsoft.VCToolsVersion.default.txt)
-export CC=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Tools/$tools_ver/bin/Hostx64/x64/cl.exe
-export CXX=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Tools/$tools_ver/bin/Hostx64/x64/cl.exe
+export CC=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Tools/MSVC/$tools_ver/bin/Hostx64/x64/cl.exe
+export CXX=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Tools/MSVC/$tools_ver/bin/Hostx64/x64/cl.exe
 ./cmake_generate.sh build Release
 cd build
 ../ninja

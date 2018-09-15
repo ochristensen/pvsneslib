75401b301039c49bfc3c53f44d630bbf6441e28f
owen christensen <humanitas@gmail.com>
2018-09-15 12:59:30 -0400

    cl

--- build_win.sh
+++ build_win.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 curl -L https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip > ninja-win.zip
-unzip -oqq ninja-win.zip
-export PATH="$(pwd)":$PATH
-export CC=cl
-export CXX=cl
+unzip -o ninja-win.zip
+export tools_ver=$(cat /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Auxiliary/Build/Microsoft.VCToolsVersion.default.txt)
+export CC=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Tools/$tools_ver/bin/Hostx64/x64/cl.exe
+export CXX=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Tools/$tools_ver/bin/Hostx64/x64/cl.exe
 ./cmake_generate.sh build Release
 cd build
 ../ninja

62b545b67b74fa6c61c61d2531af320d5ca25f2f
Owen Christensen <ochristensen@users.noreply.github.com>
2019-11-26 23:10:49 -0500

    Update build_win.sh

--- build_win.sh
+++ build_win.sh
@@ -3,7 +3,7 @@
 curl -L https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip > ninja-win.zip
 unzip -o ninja-win.zip
 export PATH="$(pwd)":$PATH
-call "/c/Program\ Files\ (x86)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat"
+cmd "/C /c/Program\ Files\ (x86)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat"
 export CC=cl.exe
 export CXX=cl.exe
 #export PATH="/c/Program\ Files\ \(x86\)/Windows\ Kits/10/bin/10.0.15063.0/x64":$PATH

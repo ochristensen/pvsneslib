6318cf62ebaae27edf6ce75b92746a748da6faab
Owen Christensen <ochristensen@users.noreply.github.com>
2019-11-26 23:00:43 -0500

    Update build_win.sh
    try itk solution

--- build_win.sh
+++ build_win.sh
@@ -3,6 +3,9 @@
 curl -L https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip > ninja-win.zip
 unzip -o ninja-win.zip
 export PATH="$(pwd)":$PATH
+call "/c/Program\ Files\ (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat"
+export CC=cl.exe
+export CXX=cl.exe
 #export PATH="/c/Program\ Files\ \(x86\)/Windows\ Kits/10/bin/10.0.15063.0/x64":$PATH
 #export tools_ver=$(cat /c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Auxiliary/Build/Microsoft.VCToolsVersion.default.txt)
 #export PATH="/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Enterprise/VC/Tools/MSVC/$tools_ver/bin/Hostx64/x64":$PATH
